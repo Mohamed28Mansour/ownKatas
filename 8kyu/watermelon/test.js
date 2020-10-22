7bac6f8c564ec1779ba2e7d53fb334254970ef8d
Mohamed Mansour <mohamed28mansour@gmail.com>
2020-10-22 16:28:12 +0200

    initiate folders and finish 1st 8kyu kata

---
+++ 8kyu/watermelon/test.js
@@ -0,0 +1,8 @@
+Test.assertEquals(divide(4), true);
+Test.assertEquals(divide(2), false);
+Test.assertEquals(divide(5), false);
+Test.assertEquals(divide(88), true);
+Test.assertEquals(divide(100), true);
+Test.assertEquals(divide(67), false);
+Test.assertEquals(divide(90), true);
+Test.assertEquals(divide(10), true);
